73dbe2a974687abd57ae9383e405e8227063a05a
Ouch1978 <ouch1978@hotmail.com>
2020-12-17 00:18:05 +0800

    Modify TargetTypedCondExprSample content.

--- LibCSharp9Demo/TargetTypedCondExprSample.cs
+++ LibCSharp9Demo/TargetTypedCondExprSample.cs
@@ -10,7 +10,7 @@ namespace LibCSharp9Demo
 {
     public class TargetTypedCondExprSample
     {
-        public IVehicle BuildVehicleWithNullCoalescing( IVehicle vehicle)
+        public IVehicle BuildVehicleWithNullCoalescing( IVehicle vehicle )
         {
             return vehicle ?? new Uber(); //在 C# 9.0 之前會錯，現在可以這樣寫了。
         }
@@ -24,7 +24,9 @@ namespace LibCSharp9Demo
         {
             int? x = new Random().NextDouble() > 0.5 ? 1 : null;
 
-            IEnumerable<int> _ = x is null ? null : new int[] { 2 , 3 };
+            IEnumerable<int> _ = x is null ? new List<int>() { 0 , 1 } : new int[] { 2 , 3 };
+
+            var another = x is null ? ( int? ) null : 3; //當使用 var 時，冒號兩邊的變數必需是相同的型別
         }
     }
 }
